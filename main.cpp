8b23431cfc14bb8605c737c77c9cd5a9667a46b9
Steven <parrsm18@wfu.edu>
2019-09-05 15:21:56 -0400

    Used Vectors

--- main.cpp
+++ main.cpp
@@ -1,29 +1,31 @@
+#include <vector>
 #include <iostream>
 using namespace std;
 
 int main() {
     const int NUM_ELEMENTS = 8;   // Number of elements
-    int revVctr[8];               // User values
+    vector<int> revVctr(NUM_ELEMENTS);               // User values
     int i;
     int tempVal = 0;// Loop index
 
     cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cin >> revVctr[i];
+        cin >> revVctr.at(i);
     }
 
     // Reverse
+
     for (i = 0; i < NUM_ELEMENTS / 2; ++i) {
-        tempVal = revVctr[NUM_ELEMENTS - 1 - i];
-        revVctr[NUM_ELEMENTS - 1 - i] = revVctr[i];
-        revVctr[i] = tempVal;
+        tempVal = revVctr.at(NUM_ELEMENTS - 1 - i);
+        revVctr.at(NUM_ELEMENTS - 1 - i) = revVctr.at(i);
+        revVctr.at(i) = tempVal;
         // Swap
     }
 
     // Print values
     cout << endl << "New values: ";
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cout << " " << revVctr[i];
+        cout << " " << revVctr.at(i);
     }
     cout << endl;
 
